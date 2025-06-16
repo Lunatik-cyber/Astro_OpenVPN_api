9a3f3c39d2b2c56cd01e1ce324d9de71026813ba
AstroFic <astrofic.guru@gmail.com>
2025-06-16 16:59:31 +0500

    middleware

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from api.routers import keys, configs, sessions, statistics, settings, system
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="OpenVPN Management API")
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # или ["http://127.0.0.1:8000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(keys.router, prefix="/keys", tags=["Keys"])
 app.include_router(configs.router, prefix="/configs", tags=["Configs"])
 app.include_router(sessions.router, prefix="/sessions", tags=["Sessions"])
